868be23dc799e6d30aeeee37e2256eb8e61893e9
lammai <67616507+lammai@users.noreply.github.com>
2020-07-15 17:39:52 -0700

    modified:   bin/Controller$1.class deleted:    bin/Controller$2.class modified:   bin/Controller.class modified:   bin/layout.fxml modified:   src/Controller.java modified:   src/layout.fxml

--- src/Controller.java
+++ src/Controller.java
@@ -22,10 +22,13 @@ public class Controller implements Initializable{
 	public ToggleGroup fractalType;
 	public Label currentItr;
 	public Label timeTaken;
-	private int maxIterations = 30;	// default iteration
-	private double ca;				// real component
-	private double cb;				// imaginary component
 	
+	private int maxIterations = 50;	// default iteration
+	private int x, y, n;
+	private double a = 0;
+	private double b = 0;
+	private double xtemp;
+	private double xP, yP;
 	
 	private double map(double n, double start1, double stop1, double start2, double stop2) {
 		return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
@@ -35,42 +38,40 @@ public class Controller implements Initializable{
 		maxIterations = Integer.parseInt(iterationInput.getText());
 	}
 	
-	public void handleRadioClicked() {
+	public void setFractalParam() {
 		RadioButton selected = (RadioButton) fractalType.getSelectedToggle();
-		if (selected.getId().equals("mandelb")) {
-			paintMandelbrot();
-		}
-		else {
-			paintJulia();
+		switch (selected.getId()) {
+		default:
+		case "mandelb":
+			a = map(x, 0, canvas.getWidth(), -2, 1);
+			b = map(y, 0, canvas.getHeight(), -1.5, 1.5);
+			xP = 0;
+			yP = 0;
+			break;
+		case "julia":
+			xP = map(x, 0, canvas.getWidth(), -1.5, 1.5);
+			yP = map(y, 0, canvas.getHeight(), -1.5, 1.5);
+			break;
 		}
 	}
 	
-	private void paintMandelbrot() {
+	private void paintFractal() {
 		new AnimationTimer() {
 			public void handle(long currentNanoTime) {
 				currentItr.setText(Integer.toString(maxIterations));
 				long startTime = System.currentTimeMillis();
-				
-				int x, y, n;
-				double a = 0;
-				double b = 0;
-				double xtemp;
-				double xP, yP;
-				
+				canvas.addEventFilter(MouseEvent.MOUSE_MOVED, e -> {
+					a = map(e.getX(), 0, canvas.getWidth(), -1, 1);
+					b = map(e.getY(), 0, canvas.getHeight(), -1, 1);
+				});
 				for (x = 0; x < canvas.getWidth(); x++) {
 					for (y = 0; y < canvas.getHeight(); y++) {
-						a = map(x, 0, canvas.getWidth(), -2, 1);
-						b = map(y, 0, canvas.getHeight(), -1.5, 1.5);
-						
-						xP = 0;
-						yP = 0;
-						
+						setFractalParam();
 						n = 0;
 						while(xP * xP + yP * yP <= 4 && n < maxIterations) {
 							xtemp = xP * xP - yP * yP + a;
 							yP = 2 * xP * yP + b;
 							xP = xtemp;
-							
 							n++;
 						}
 						if (n < maxIterations) {
@@ -85,56 +86,6 @@ public class Controller implements Initializable{
 						}
 					}
 				}
-				
-				timeTaken.setText(System.currentTimeMillis() - startTime + " milliseconds");
-			}
-		}.start();
-	}
-	
-	private void paintJulia() {	// Julia set 
-		new AnimationTimer() {
-			public void handle(long currentNanoTime) {
-				currentItr.setText(Integer.toString(maxIterations));
-				
-				canvas.addEventFilter(MouseEvent.MOUSE_MOVED, e -> {	// Animate Julia set with
-					ca = map(e.getX(), 0, canvas.getWidth(), -1, 1);	// mouse coordinate
-					cb = map(e.getY(), 0, canvas.getHeight(), -1, 1);
-				});
-				
-				
-				long startTime = System.currentTimeMillis();
-				
-				int x, y, n;
-				double a = 0;
-				double b = 0;
-				double xtemp;
-				
-				for (x = 0; x < canvas.getWidth(); x++) {
-					for (y = 0; y < canvas.getHeight(); y++) {
-						a = map(x, 0, canvas.getWidth(), -1.5, 1.5);
-						b = map(y, 0, canvas.getHeight(), -1.5, 1.5);
-						
-						n = 0;
-						while(a * a + b * b <= 4 && n < maxIterations) {
-							xtemp = a * a - b * b + ca;
-							b = 2 * a * b + cb;
-							a = xtemp;
-							
-							n++;
-						}
-						if (n < maxIterations) {
-							double newC = ((double)n)/((double)maxIterations);
-							Color c;
-							if(newC > 0.4) c = Color.color(1, newC, 0);
-							else c = Color.color(newC, 0, Math.abs(newC-0.3));
-							pw.setColor(x, y, c);
-						}
-						else {
-							pw.setColor(x, y, Color.BLACK);
-						}
-					}
-				}
-				
 				timeTaken.setText(System.currentTimeMillis() - startTime + " milliseconds");
 			}
 		}.start();
@@ -143,6 +94,6 @@ public class Controller implements Initializable{
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		pw = canvas.getGraphicsContext2D().getPixelWriter();
-		paintJulia();
+		paintFractal();
 	}
 }
